2f178cc83bff24d913e6ac2a58551ceb2f49d63a
Vivian Li <vivl@microsoft.com>
2020-08-14 14:37:53 -0700

    Add an image

--- main.ts
+++ main.ts
@@ -1,3 +1,25 @@
+namespace SpriteKind {
+    export const Effect = SpriteKind.create()
+    export const TeleportEffect = SpriteKind.create()
+}
+let teleportEffectSprite: Sprite = null
 function teleportEffect (sprite: Sprite) {
-	
+    teleportEffectSprite = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . 9 . . . . . . . . . . . . . 
+        . . 9 . . . . . . . . 9 . . . . 
+        . . 9 . . . . . . . . 9 . . . . 
+        . . . . . . . 9 . . . 9 . . . . 
+        . . . . . . . 9 . . . 9 . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . 9 . . 
+        . . . . . 9 . . . . . . . 9 . . 
+        . . . . . 9 . . . . . . . 9 . . 
+        . . . . . 9 . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.TeleportEffect)
 }
